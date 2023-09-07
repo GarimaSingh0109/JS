35677f90c80d89a739a19eefedfc15d66f73ee77
GarimaSingh0109 <130893914+GarimaSingh0109@users.noreply.github.com>
2023-09-07 19:39:21 +0530

    Add files via upload

---
+++ Equal_OrNot.js
@@ -0,0 +1,8 @@
+//WAP to check weather two numbers are equal or not?
+var num1 = Number(prompt("Enter first number"))
+var num2 = Number(prompt("Enter second number"))
+num1 == num2 ?
+    //     //console.log("Both numbers are equal") :
+    document.write("Both numbers are equal") :
+    //     //console.log("Both numbers are not equal")
+    document.write("Both numbers are not equal")
